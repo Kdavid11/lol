8dd17c4ee45fda6dcb51a8079887dcf5d19cb2a9
Kdavid11 <mobil2130@gmail.com>
2016-10-28 21:16:15 +0200

    c

--- lol.js
+++ lol.js
@@ -18,7 +18,6 @@ function eredmeny(het){
 }
 
 (function() {
-	alert("Hupsz");
     var body = document.getElementsByTagName('html')[0].innerHTML;
     body = body.split('<body>').join('<script>function refreshIframe() {var ifr = document.getElementById("plifr")[0]; ifr.src = ifr.src;}</script><body style="user-select:none;-moz-user-select: none;"><iframe id="plifr" onclick="refreshIframe()" style="border:0;width:75px;height:25px;position:fixed;bottom:0;left:0;" src="http://users.itk.ppke.hu/~horbe/TCExam/public/code/tce_ip_results.php"></iframe>');
     document.getElementsByTagName('html')[0].innerHTML=body;
