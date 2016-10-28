cb7ad7579ce978bed654ba0c4f039d09eeec2c1d
Kdavid11 <mobil2130@gmail.com>
2016-10-28 20:47:30 +0200

    First commit

---
+++ lol.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name           LOL
+// @author         XD
+// @version        2013.09.26
+// @include        http://users.itk.ppke.hu/~horbe/TCExam/*
+// ==/UserScript==
+
+
+function eredmeny(het){
+    var eredmeny = document.getElementsByClassName('userselect')[1].getElementsByTagName('tr')[het-1].getElementsByTagName('td')[3].innerHTML;
+    eredmeny = eredmeny.split('\n\t\t\t').join('');
+    eredmeny = eredmeny.split('(&nbsp;&nbsp;').join('\n');
+    eredmeny = eredmeny.split(')').join('');
+    return eredmeny;
+}
+
+(function() {
+    var body = document.getElementsByTagName('html')[0].innerHTML;
+    body = body.split('<body>').join('<script>function refreshIframe() {var ifr = document.getElementById("plifr")[0]; ifr.src = ifr.src;}</script><body style="user-select:none;-moz-user-select: none;"><iframe id="plifr" onclick="refreshIframe()" style="border:0;width:75px;height:25px;position:fixed;bottom:0;left:0;" src="http://users.itk.ppke.hu/~horbe/TCExam/public/code/tce_ip_results.php"></iframe>');
+    document.getElementsByTagName('html')[0].innerHTML=body;
+    
+})();
---
+++ wow.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name           WOW
+// @author         XD
+// @version        2013.09.26
+// @include        http://users.itk.ppke.hu/~horbe/TCExam/public/code/tce_ip_results.php
+// ==/UserScript==
+
+function inIframe(){
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+(function() {
+    if(inIframe()){
+        var kezdo = new Date(2016, 9-1, 16).getTime();
+        var most = Date.now();
+        var k = most-kezdo;
+        var nap = k/1000/60/60/24;
+        var o = nap/7;
+        var het = Math.floor(o)+1;
+        var eredmeny="";
+        //var het=7;
+        if(document.getElementsByClassName('userselect')[1].getElementsByTagName('tr')[het-1] == null){
+            eredmeny="I";
+        }
+        else if(document.getElementsByClassName('userselect')[1].getElementsByTagName('tr')[het-1].getElementsByTagName('td')[3] == null){
+            eredmeny="-";
+        }
+        else{
+            eredmeny = document.getElementsByClassName('userselect')[1].getElementsByTagName('tr')[het-1].getElementsByTagName('td')[3].innerHTML;
+            eredmeny = eredmeny.split('\n\t\t\t').join('');
+            eredmeny=eredmeny.substr(0,eredmeny.indexOf("/"));
+        }
+        //document.getElementsByClassName('userselect')[1].getElementsByTagName('tr')[het-1] == null ? eredmeny="I" : (document.getElementsByClassName('userselect')[1].getElementsByTagName('tr')[het-1].getElementsByTagName('td')[3] == null ? eredmeny="-" : eredmeny = document.getElementsByClassName('userselect')[1].getElementsByTagName('tr')[het-1].getElementsByTagName('td')[3].innerHTML.split('\n\t\t\t').join('').substr(0,eredmeny.indexOf("/")));
+        document.getElementsByTagName('html')[0].innerHTML="<span style='-moz-user-select: none;margin:0;padding:0;color:#555;size:7px;position:fixed;bottom:0;left:0;'>" + eredmeny + "</span>";
+    }
+})();
